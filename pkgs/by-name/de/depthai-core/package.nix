450893c3c9518359494e2c4c8d25b4bc44af8e9f
Anton Tetov <anton@tetov.se>
2025-09-16 16:10:50 +0200

    depthai-core: wip

--- pkgs/by-name/de/depthai-core/package.nix
+++ pkgs/by-name/de/depthai-core/package.nix
@@ -63,59 +63,64 @@ stdenv.mkDerivation (finalAttrs: {
     libnop
   ];
 
-  patches = [./cmake_deps.patch];
+  patches = [ ./cmake_deps.patch ];
 
   cmakeFlags =
     let
       inherit (lib) cmakeBool cmakeFeature concatStringsSep;
-        assetFetcher = callPackage ./fetch-depthai-assets.nix { };
-        ext = ".tar.xz";
-        # from cmake/Depthai/DepthaiDeviceSideConfig.cmake and cmake/DepthaiDownloader.cmake
-        firmwareBasename = "depthai-device-fwp";
-        firmware = assetFetcher {
-          inherit ext;
-          subDir = "luxonis-myriad-snapshot-local/depthai-device-side";
-          basename = firmwareBasename;
+
+      luxonisArtifactoryUrl = "https://artifacts.luxonis.com/artifactory";
+
+      # from cmake/Depthai/DepthaiDeviceSideConfig.cmake and cmake/DepthaiDownloader.cmake
+      firmware =
+        let
+          name = "depthai-device-fwp";
           rev = "a62b2ccb0bc493c2fb41694cb81c08887be24c52";
+        in
+        builtins.fetchurl {
+          # name = "${name}.tar.xz";
+          url = "${luxonisArtifactoryUrl}/luxonis-myriad-snapshot-local/depthai-device-side/${rev}/${name}-${rev}.tar.xz";
+
           sha256 = "sha256-njwp9WIq0OyEIsHx4hoHhbRAr+W3tl/PnmQ77OBgbZc=";
         };
 
-        # from cmake/DepthaiBootloaderDownloader.cmake and cmake/Depthai/DepthaiBootloaderConfig.cmake
-        bootloaderBasename = "depthai-bootloader-fwp";
-        bootloader = assetFetcher {
-          inherit ext;
-          subDir = "luxonis-myriad-release-local/depthai-bootloader";
-          basename = bootloaderBasename;
+      # from cmake/DepthaiBootloaderDownloader.cmake and cmake/Depthai/DepthaiBootloaderConfig.cmake
+      bootloader =
+        let
+          name = "depthai-bootloader-fwp";
           rev = "0.0.28";
+        in
+        builtins.fetchurl {
+          # name = "${name}.tar.xz";
+          url = "${luxonisArtifactoryUrl}/luxonis-myriad-release-local/depthai-bootloader/${rev}/${name}-${rev}.tar.xz";
           sha256 = "sha256-5xQ7vj0BLt2dgZQKC+Ug6BVkcIt7TsebBx1wF1i7gow=";
         };
     in
     [
       (cmakeBool "HUNTER_ENABLED" false)
       (cmakeBool "DEPTHAI_ENABLE_BACKWARD" false)
-      (cmakeFeature "FP16_DIR"
-        (toString writeTextFile {
-                name = "FP16Config";
-                text = ''
-                  if(NOT TARGET FP16::fp16)
-                    add_library(FP16::fp16 INTERFACE IMPORTED)
-                    target_include_directories(FP16::fp16 INTERFACE "${fp16}/include")
-                  endif()
-                '';
-                destination = "/FP16Config.cmake";
-              }
-        )
-      )
-      (cmakeFeature "libnop_DIR"
-        (toString writeTextFile {
-              name = "libnop-config";
-              text = ''
-                add_library(libnop INTERFACE IMPORTED)
-                target_include_directories(libnop INTERFACE "${libnop}/include")
-              '';
-              destination = "/libnopConfig.cmake";
+      (cmakeFeature "FP16_DIR" (
+        toString (writeTextFile {
+          name = "FP16Config";
+          text = ''
+            if(NOT TARGET FP16::fp16)
+              add_library(FP16::fp16 INTERFACE IMPORTED)
+              target_include_directories(FP16::fp16 INTERFACE "${fp16}/include")
+            endif()
+          '';
+          destination = "/FP16Config.cmake";
         })
-      )
+      ))
+      (cmakeFeature "libnop_DIR" (
+        toString (writeTextFile {
+          name = "libnop-config";
+          text = ''
+            add_library(libnop INTERFACE IMPORTED)
+            target_include_directories(libnop INTERFACE "${libnop}/include")
+          '';
+          destination = "/libnopConfig.cmake";
+        })
+      ))
       (cmakeBool "DEPTHAI_ENABLE_CURL" false) # only used for telemetry and zoo
       (cmakeBool "CMAKE_SKIP_INSTALL_ALL_DEPENDENCY" true)
       (cmakeFeature "CMAKE_INSTALL_PREFIX" "${placeholder "out"}")
